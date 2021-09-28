e47c4a07fbbc1aa8472f1b87f486c8d90424889d
Szymon Mrzyglod <szymon.mr@gmail.com>
2021-09-28 10:21:19 +0200

    add addTask method

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TaskList from './components/TaskList/TaskList';
 import './App.css';
 
 class App extends Component{
+  counter = 6;
   state={
     tasks: [
       {
@@ -53,7 +54,7 @@ class App extends Component{
         important: true,
         active: true,
         finishDate: null
-      },
+      }
     ]
   }
   deleteTask = (id) => {
@@ -78,11 +79,27 @@ class App extends Component{
       tasks,
     })
   }
+  addTask = (text, date, important) => {
+    console.log("Data w add" + date);
+    const task = {
+      id: this.counter,
+      text,
+      date,
+      important,
+      active: true,
+      finishDate: null
+    }
+    this.counter++;
+    this.setState(prevState => ({
+      tasks: [...prevState.tasks, task]
+    }))
+    return true;
+  }
   render(){
     return (
       <div className="App">
-        to do app
-        <AddTask />
+        <h1>TO DO APP</h1>
+        <AddTask add={this.addTask}/>
         <TaskList tasks={this.state.tasks} delete={this.deleteTask} change={this.changeTaskStatus}/>
       </div>
     );
--- src/components/AddTask/AddTask.js
+++ src/components/AddTask/AddTask.js
@@ -1,17 +1,57 @@
 import React, {Component} from 'react'
-
+import './AddTask.css'
 class AddTask extends Component {
+    state = {
+        text: '',
+        checked: false,
+        date: new Date().toISOString().slice(0, 10),
+    }
+    handleChangeInput = e => {
+        this.setState({
+            text: e.target.value
+        })
+    }
+    handleDate = e => {
+        this.setState({
+            date: e.target.value
+        })
+    }
+    handleCheck = e => {
+        this.setState({
+            checked: e.target.checked
+        })
+    }
+    handleClick = () => {
+        const {text, checked, date} = this.state;
+        if(text.length>2){
+        const add = this.props.add(text, date, checked)
+        console.log(date);
+            if(add){
+                this.setState({
+                    text: '',
+                    checked: false,
+                    date: new Date().toISOString().slice(0, 10),
+                })
+            }  
+        }else{
+            console.log('Za krótko');
+        }
+
+    }
     render() { 
+        const minDate = new Date().toISOString().slice(0, 10);
+        let maxDate = Number(minDate.slice(0, 4)) + 1;
+        maxDate = maxDate + minDate.slice(4, 10);
         return (
             <div className="form">
-                <div>
-                    DODAJ ZADANIE
-                </div>
-                <hr />
+                <input type="text" placeholder="Dodaj zadanie..." value={this.state.text} onChange={this.handleChangeInput}/>
+                <input type="checkbox" checked={this.state.checked} id="important" onChange={this.handleCheck}/>
+                <label htmlFor="important">Priorytet</label><br />
+                <label htmlFor="date">Do kiedy zrobić</label>
+                <input type="date" value={this.state.date} min={minDate} max={maxDate} onChange={this.handleDate}/><br />
+                <button onClick={this.handleClick}>Dodaj</button>
             </div>
-        )
-        
-     
+        )  
     }
 }
  
